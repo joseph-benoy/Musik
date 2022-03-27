ca7937e17510c5e07b6733d4572099e39ce4b50d
joseph-benoy <josephbenoy03@gmail.com>
2022-03-27 14:40:23 +0530

    added delete and fav button to list

--- src/elements/SongList/index.jsx
+++ src/elements/SongList/index.jsx
@@ -1,12 +1,20 @@
-import { ListGroup,ListGroupItem } from "react-bootstrap";
+import { ListGroup,ListGroupItem,Button } from "react-bootstrap";
 import "./songlist.css";
 import React from "react";
+import { Heart, Trash } from "react-bootstrap-icons";
 function SongList({songs}) {
     return ( 
         <ListGroup className="songListWrapper">
         {
             songs.map((song)=>(
-                <ListGroupItem>{song.TITLE}</ListGroupItem>
+                <ListGroupItem action as="li"
+                className="d-flex justify-content-between align-items-start">
+                    <div className="ms-2 me-auto">
+                        {song.TITLE}
+                    </div>
+                    <Button variant="link"><Heart/></Button>
+                    <Button variant="link"><Trash/></Button>
+                </ListGroupItem>
             ))
         }
         </ListGroup>
